019e5e92de2bb4b0d39175646bd18330cc215d34
Divyansh Raj <rajdivyansh2000@gmail.com>
2025-06-06 03:08:04 +0530

    style changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,14 @@
 import ticTacToe from "../assets/images/ticTacToe.png";
 const Header = () => {
   return (
-    <>
+    <div className="headerContainer">
       <img src={ticTacToe} alt="Tic Tac Toe" height={50} width={50} />
       <header>
-        <h1>Tic Tac Toe</h1>
+        <span>
+          <strong>Tic Tac Toe</strong>
+        </span>
       </header>
-    </>
+    </div>
   );
 };
 
